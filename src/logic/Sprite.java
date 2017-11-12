d3f3835f3b966761e7a02010c9801257508a4a1a
peterquacks <Peter@Peters-MacBook-Pro.local>
2017-11-12 13:03:58 -0700

    Sprite

---
+++ src/logic/Sprite.java
@@ -0,0 +1,46 @@
+package logic;
+import java.awt.Image;
+
+public abstract class Sprite {
+	public static enum State{
+		IDLE, MOVING_LEFT, MOVING_RIGHT, MOVING_DOWN, MOVING_UP
+	};
+	
+	//current state and frame
+	protected State state;
+	protected int frame;
+	
+	//Constructor. init at idle
+	public Sprite() {
+		state = state.IDLE;
+		frame = 0;
+	}
+	
+	//getteres and setters
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	public State getState() {
+		return state;
+	}
+	
+	//animation
+	public boolean isFinished(){
+		return false;
+	}
+	
+	//resets animation
+	public void reset() {}
+	
+	//gets image for sprite
+	public abstract Image getImage();
+	
+	public abstract int getWidth();
+	
+	public abstract int getHeight();
+	
+	
+	
+	
+}
