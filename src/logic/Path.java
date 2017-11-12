c8ba416b3049edadcd632f49c29cf9c9f38ea5ca
nyokley <nyokley@hotmail.com>
2017-11-12 14:51:28 -0700

    Updated Path
    Implemented findPos

--- src/logic/Path.java
+++ src/logic/Path.java
@@ -1,13 +1,30 @@
 package logic;
 
-public class Path {
-	private int[][] path;
+import java.util.*;
 
-	public int[][] getPath() {
+public class Path {
+	private ArrayList<int[][]> path;
+	
+	public Path(){
+		path = new ArrayList<int[][]>();
+	}
+
+	public ArrayList<int[][]> getPath() {
 		return path;
 	}
 
-	public void setPath(int[][] path) {
-		this.path = path;
+	public void setPath(ArrayList<int[][]> coordinates) {
+		this.path = coordinates;
 	}
+	
+	public int[][] findPos(int step) {
+		if(step < path.size() && step >= 0) {
+			return path.get(step);
+		}
+		else {
+			return null;
+		}
+	}
+
+	
 }
